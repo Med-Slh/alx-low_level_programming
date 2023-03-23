3c75ff3ad9e89fa255097925845fada9093963c3
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-23 16:34:26 -0700

    x10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers
+ *
+ * return: always 0
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	char a;
+
+	while (i <= 0)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar(a + 48);
+		}
+		_putchar('\n');
+	}
+}
