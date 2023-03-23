2ecc5bcef2149085a0e6d3c94f8f5a8f6f4b7472
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-23 15:30:51 -0700

    isupcase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _isupper(int c) - function that return 1 or 0
+ * _isupper - function that return 1 or 0
  * @c: c is an integer parameter
  * Return: 1 or 0
  */
 int _isupper(int c)
 {
-        if (c >= 65 && c <= 90)
-        {
-                return (1);
-        }
-	else
-        return (0);
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
