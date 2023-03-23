a5a96ba00b01363d1d32bae5983335db26b8be9d
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-23 16:13:03 -0700

    except 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 - 9
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + 48);
+	}
+	_putchar('\n');
+}
