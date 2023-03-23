9b59d345050bbe5652b5e55628b391064c385444
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-23 15:46:47 -0700

    muli 2 int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: a is an integer parameter
+ * @b: b is an integer parameter
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
