3a222892cb6c0643a90676f98a2fbc87becf1853
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-23 16:02:42 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,9 @@ void print_numbers(void)
 {
 	int a;
 
-		for (a = 48; a <= 57; a++)
+		for (a = 0; a <= 9; a++)
 		{
-			_putchar(a);
+			_putchar(a + 48);
 		}
 		_putchar('\n');
-		return (0);
 }
