4f39fc68bd8d00f110182d13cb6992b252614b05
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-23 16:37:26 -0700

    x10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 /**
  * more_numbers - prints 10 times the numbers
  *
- * return: always 0
+ * Return: always 0
  */
 
 void more_numbers(void)
@@ -10,12 +10,14 @@ void more_numbers(void)
 	int i = 0;
 	char a;
 
-	while (i <= 0)
+	while (i <= 9)
 	{
 		for (a = 0; a <= 14; a++)
 		{
 			_putchar(a + 48);
 		}
 		_putchar('\n');
+
+		i++;
 	}
 }
