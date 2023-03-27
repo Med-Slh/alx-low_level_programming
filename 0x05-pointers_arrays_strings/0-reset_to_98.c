15c7faf3578ebf5dd200613da96306327e308f48
Med-Slh <mohamed.salhaouii@gmail.com>
2023-03-27 16:20:02 -0700

    swiptwo int

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - reset to int
+ * reset_to_98 - reset int to 98
  *
  * @n: int parameter
  * Return: always 0
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,6 +10,7 @@
 
 void swap_int(int *a, int *b)
 {
-	*b = a;
+	temp = *a;
 	*a = b;
+	*b = temp;
 }
