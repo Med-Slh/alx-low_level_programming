74e2b51ab3319375e43bb00d6101c5ffa8381eef
Med-Slh <mohamed.salhaouii@gmail.com>
2023-04-19 18:44:22 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "3-clac."
+#include "3-calc.h"
 #include <stdlib.h>
 /**
  * get_op_func - select the correct func
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,9 +1,9 @@
-#include "3-clac.h"
+#include "3-calc.h"
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
-int op_ mod(int a, int b);
+int op_mod(int a, int b);
 
 /**
  * op_add - returns the sum of two numbers
