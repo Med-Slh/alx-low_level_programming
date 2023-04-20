ef74b61782b80a98573829e3cad6ac8f730060ae
Med-Slh <mohamed.salhaouii@gmail.com>
2023-04-19 17:14:30 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that print a name
+ * @name: the name
+ * @f: the printing fonction
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
