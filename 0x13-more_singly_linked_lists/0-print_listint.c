165a914d862f2284ee9894d0aa9309cd18bb6cff
Med-Slh <mohamed.salhaouii@gmail.com>
2023-05-01 07:23:20 -0700

    00

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return(0);
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * print_listint - print all the elements of list
+ * @h: list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
