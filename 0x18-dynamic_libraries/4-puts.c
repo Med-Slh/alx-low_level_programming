a8a77ddcd0abdc85581eef51ba64c4cceeb77db6
Med-Slh <mohamed.salhaouii@gmail.com>
2023-06-19 14:16:15 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/4-puts.c
+++ 0x18-dynamic_libraries/4-puts.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
+#include "main.h"
 /**
- * main - Entry point
+ * _puts - print a string to stdout
  *
- * Return: Always 0 (Success)
+ * @str: parametre de type char
+ * Return: Always 0
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
 }
