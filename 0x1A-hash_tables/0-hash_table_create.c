65e23ca91c483b36668b5e4dfce829c8d3ca5a86
Med-Slh <mohamed.salhaouii@gmail.com>
2023-07-08 05:37:30 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * *hash_table_create - a function that creat a hash table
+ * hash_table_create - a function that creat a hash table
  *
  * @size: The size of the array
  * Return: return a pointure or NULL
@@ -12,18 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	if (hash_table == NULL)
-	{
 		return (NULL);
-	}
+
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
-	{
 		return (NULL);
-	}
+
 	hash_table->size = size;
 	for (i = 0; i < size; i++)
-	{
 		hash_table->array[i] = NULL;
-	}
 	return (hash_table);
 }
