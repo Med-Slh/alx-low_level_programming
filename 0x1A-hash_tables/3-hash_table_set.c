cdfecad83d01b2ed41389e014d02be6202b0f676
Med-Slh <mohamed.salhaouii@gmail.com>
2023-07-08 12:05:17 -0700

    update task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,12 +10,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	 hash_node_t *new_node;
+	 hash_node_t *new_node, *current;
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 
