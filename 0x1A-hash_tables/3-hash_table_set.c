4c60118b6d7223a27ed0d45e414a4a30574ea814
Med-Slh <mohamed.salhaouii@gmail.com>
2023-07-08 08:12:48 -0700

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_set - function that adds an element to the HT
  * @ht: hash table
@@ -8,14 +9,15 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	 hash_node_t *new_node;
+
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 
-	unsigned long int index;
+	index = key_index((const unsigned char *)key, ht->size);
 
-	index = hash_djb2((const unsigned char *)key, ht->size);
-
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
 		return (0);
@@ -26,8 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
-		fre(new_node->value);
-		fre(new_node);
+		free(new_node->value);
+		free(new_node);
 		return (0);
 	}
 	new_node->next = ht->array[index];
